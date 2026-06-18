185f466b1a978eb03be9085dbddf7a6802c30e0e
Ashutosh Pratap Mall <ashupratap.26@gmail.com>
2026-06-18 14:57:53 +0530

    updated backend

--- Backend/utils/token.js
+++ Backend/utils/token.js
@@ -2,14 +2,16 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
   try {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
 
+    const isProduction = process.env.NODE_ENV === "production";
+
     const cookieOptions = {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: isProduction, 
+      sameSite: isProduction ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     };
 
